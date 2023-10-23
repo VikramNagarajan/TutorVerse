f244245a0aff6f1b9f2f1f2f213b4563c9712422
Eric <eric.c12333@gmail.com>
2023-10-22 22:38:31 -0400

    echen commit 2

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,16 +1,36 @@
 const Services = () => {
   return (
-    <div id="services" className="flex flex-col items-center font-rubik w-full justify-center">
-      <section  className="justify-center">
-        <div className="text-4xl font-semibold">
-            Services
+    <div id="services" className="flex flex-col font-rubik w-full">
+      <div className="text-4xl font-semibold text-center text-white">
+        Services
+      </div>
+      <div className="flex flex-row justify-center pt-12">
+        <div className="card card-side bg-green-700 shadow-xl text-xl w-3/12 " style={{ marginRight: '6rem' }}>
+          {/* <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure> */}
+          <div className="card-body">
+            <h2 className="card-title text-2xl text-white">Formulate Study Groups</h2>
+            <p> Easily find and join study groups with peers who are in the same class.
+              Collaborate, discuss, and review course materials together to enhance your learning experience.</p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-accent text-black hover:scale-x-125 hover:scale-y-125">Try for free!</button>
+            </div>
+          </div>
         </div>
-        <div className="font-semibold">
-            Match with others
+        <div className="card card-side bg-teal-700 shadow-xl text-xl w-3/12">
+         {/*  <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie" /></figure> */}
+          <div className="card-body">
+            <h2 className="card-title text-2xl text-white">Tutoring Assistance</h2>
+            <p> Connect with experienced tutors in your class for one-on-one or group tutoring sessions.
+                Get personalized help and support for challenging topics and assignments.
+            </p>
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary hover:scale-x-125 hover:scale-y-125">Try for free!</button>
+            </div>
+          </div>
         </div>
-      </section>
+      </div>
     </div>
   )
 }
 
-export default Services
+export default Services;
